9063953213f1f9a39f4c7abd0d5a44387487fb29
Nikita Gryzlov <nixel2007@gmail.com>
2019-10-28 17:08:37 +0300

    Target 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,11 @@ repositories {
 group = "com.github.1c-syntax"
 version = gitVersionCalculator.calculateVersion("v")
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
